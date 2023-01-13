c21d850471f55bcb8e69d58e6961ec99de21ef1d
Hamid.RM <hamidali.reza41@outlook.com>
2023-01-13 15:01:39 +0330

    added resource files and custom RequiredAttribute

---
+++ src/MyWebsite.Shared/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,15 @@
+﻿using MyWebsite.Shared.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebsite.Shared.Attributes
+{
+	public class MyRequiredAttribute : RequiredAttribute
+	{
+		public MyRequiredAttribute()
+		{
+			ErrorMessageResourceType = typeof(ErrorResource);
+			ErrorMessageResourceName = nameof(ErrorResource.Required);
+			ErrorMessage = nameof(ErrorResource.Required);
+		}
+	}
+}
