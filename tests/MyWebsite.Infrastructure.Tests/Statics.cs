668dfa8b1b6181123cf47cea61d4f15a1922fc2e
Hamid.RM <hamidali.reza41@outlook.com>
2023-02-16 23:41:36 +0330

    added SqlServer Container in githubActions

--- tests/MyWebsite.Infrastructure.Tests/Statics.cs
+++ tests/MyWebsite.Infrastructure.Tests/Statics.cs
@@ -1,6 +1,5 @@
 namespace MyWebsite.Infrastructure.Tests;
 public static class Statics
 {
-	//public const string dbConnectionString = "Server=localhost,1434;Database=mywebiste-testdb;User Id=sa;Password=Admin123@;";
-	public const string dbConnectionString = "Server=.;Database=mywebiste-testdb;Trusted_Connection=True;Trust Server Certificate=true";
+	public const string dbConnectionString = "Server=localhost,1434;Database=mywebiste-testdb;User Id=SA;Password=Abcd1234@;Trusted_Connection=False;TrustServerCertificate=True;";
 }
