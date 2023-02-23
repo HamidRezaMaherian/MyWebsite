2a463a32f45f19e44cfb5f342c958a6b2ad7b873
Hamid.RM <hamidali.reza41@outlook.com>
2023-02-23 13:10:57 +0330

    modified sql database ports

--- tests/MyWebsite.Infrastructure.Tests/Statics.cs
+++ tests/MyWebsite.Infrastructure.Tests/Statics.cs
@@ -1,5 +1,5 @@
 namespace MyWebsite.Infrastructure.Tests;
 public static class Statics
 {
-	public const string dbConnectionString = "Server=localhost,1434;Database=mywebiste-testdb;User Id=SA;Password=Abcd1234@;Trusted_Connection=False;TrustServerCertificate=True;";
+	public const string dbConnectionString = "Server=localhost;Database=mywebiste-testdb;User Id=SA;Password=Abcd1234@;Trusted_Connection=False;TrustServerCertificate=True;";
 }
