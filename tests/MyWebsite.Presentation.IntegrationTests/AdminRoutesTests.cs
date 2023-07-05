56c1897e614d76476925da65f087a2cf27b771ed
Hamid.RM <hamidali.reza41@outlook.com>
2023-07-05 19:17:45 +0330

    split Presentation test project to Unit and Integration Projects

---
+++ tests/MyWebsite.Presentation.IntegrationTests/AdminRoutesTests.cs
@@ -0,0 +1,36 @@
+﻿using MyWebsite.Presentation.Tests.Utils;
+using System.Net;
+
+namespace MyWebsite.Presentation.Admin.IntergrationTests
+{
+    [TestFixture]
+    public class AdminRoutesTests : BunitTestContext
+    {
+        private TestingWebAppFactory<Admin.Program> _application;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _application = new TestingWebAppFactory<Admin.Program>((serviceCollection) =>
+            {
+            });
+        }
+        [Test]
+        public async Task HealthCheckEndpoint_ReturnsOk()
+        {
+            _application = new TestingWebAppFactory<Admin.Program>();
+            var client = _application.CreateClient();
+            var result = await client.GetAsync("/healthstatus");
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+        [Test]
+        public async Task HomeRouteEndpoint_ReturnsOkWithProperHtmlOutput()
+        {
+            _application = new TestingWebAppFactory<Admin.Program>();
+            var client = _application.CreateClient();
+            var result = await client.GetAsync("/");
+            //var htmlOutput = TestContext!.RenderComponent<Admin.Pages.Home.Index>().Markup;
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
---
+++ tests/MyWebsite.Presentation.IntegrationTests/SiteRoutesTests.cs
@@ -0,0 +1,27 @@
+﻿using MyWebsite.Presentation.Tests.Utils;
+using System.Net;
+
+namespace MyWebsite.Presentation.IntegrationTests
+{
+    [TestFixture]
+    public class SiteRoutesTests
+    {
+        private TestingWebAppFactory<Program> _application;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _application = new TestingWebAppFactory<Program>((serviceCollection) =>
+            {
+            });
+        }
+        [Test]
+        public async Task SiteRoutesEndpoint_ReturnsOk()
+        {
+            _application = new TestingWebAppFactory<Program>();
+            var client = _application.CreateClient();
+            var result = await client.GetAsync("/");
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
